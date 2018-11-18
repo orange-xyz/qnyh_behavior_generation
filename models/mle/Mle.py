a0be9859230f860b5e0bfaa4ab1762af142800f3
orange-xyz <1213772031@qq.com>
2018-11-18 23:23:34 +0800

    LSTM prediction

--- models/mle/Mle.py
+++ models/mle/Mle.py
@@ -9,12 +9,40 @@ from utils.metrics.Nll import Nll
 from utils.oracle.OracleLstm import OracleLstm
 from utils.utils import *
 
+def my_generate_samples(sess, trainable_model, batch_size, sequence_length, generate_num, output_file=None, get_code=True, g_x_file=None):
+    # Generate Samples
+    generated_samples = []
+    data_loader = DataLoader(batch_size=batch_size, seq_length=sequence_length)
+    data_loader.create_batches(data_file=g_x_file)
+
+    for _ in range(generate_num // batch_size):
+        g_x = data_loader.next_batch()
+        g_x = g_x.reshape(batch_size, sequence_length)
+        generated_samples.extend(trainable_model.generate(sess, g_x))
+    codes = []
+    if output_file is not None:
+        with open(output_file, 'w') as fout:
+            for poem in generated_samples:
+                for item in poem:
+                    buffer = (' '.join([str(x) for x in item]) + '\n').replace('[', '').replace(']', '')
+                    fout.write(buffer)
+                if get_code:
+                    codes.append(poem)
+        return np.array(codes)
+    # codes = ""
+    # for poem in generated_samples:
+    #     tmp = []
+    #     for item in poem:
+    #         tmp.extend(item)
+    #     buffer = ' '.join([str(x) for x in item]) + '\n'.replace('[', '').replace(']', '')
+    #     codes += buffer
+    # return codes
 
 class Mle(Gan):
     def __init__(self, oracle=None):
         super().__init__()
         # you can change parameters, generator here
-        self.vocab_size = 14994
+        self.vocab_size = 30000
         self.emb_dim = 32
         self.hidden_dim = 32
         self.sequence_length = 20
@@ -23,12 +51,13 @@ class Mle(Gan):
         self.l2_reg_lambda = 0.2
         self.dropout_keep_prob = 0.75
         self.batch_size = 64
-        self.generate_num = 128
+        self.generate_num = 1024
         self.start_token = 0
 
         self.oracle_file = 'models/mle/save/oracle.txt'
         self.generator_file = 'models/mle/save/generator.txt'
         self.test_file = 'models/mle/save/test_file.txt'
+        self.g_x_file = 'models/mle/save/g_x_file.txt'
 
     def init_oracle_trainng(self, oracle=None):
         if oracle is None:
@@ -147,26 +176,33 @@ class Mle(Gan):
             with open(self.test_file, 'w') as outfile:
                 outfile.write(code_to_text(codes=codes, dictionary=dict))
 
-        self.sess.run(tf.global_variables_initializer())
+        # self.sess.run(tf.global_variables_initializer())
 
-        self.pre_epoch_num = 80
-        self.adversarial_epoch_num = 100
-        self.log = open('experiment-log-mle-real.csv', 'w')
-        generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
-        self.gen_data_loader.create_batches(self.oracle_file)
+        # self.pre_epoch_num = 30
+        # #self.adversarial_epoch_num = 100
+        # self.log = open('experiment-log-mle-real.csv', 'w')
+        # #my_generate_samples(self.sess, self.generator, self.batch_size, self.sequence_length, self.generate_num, self.generator_file, g_x_file=self.g_x_file)
+        # self.gen_data_loader.create_batches(self.oracle_file)
 
-        print('start pre-train generator:')
-        for epoch in range(self.pre_epoch_num):
-            start = time()
-            loss = pre_train_epoch(self.sess, self.generator, self.gen_data_loader)
-            end = time()
-            print('epoch:' + str(self.epoch) + '\t time:' + str(end - start))
-            self.add_epoch()
-            if epoch % 5 == 0:
-                generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
-                get_real_test_file()
-                self.evaluate()
-        generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+        # saver=tf.train.Saver(max_to_keep=1)
+        # print('start pre-train generator:')
+        # for epoch in range(self.pre_epoch_num):
+        #     saver.save(self.sess,'ckpt/lstm.ckpt',global_step=epoch)
+        #     start = time()
+        #     loss = pre_train_epoch(self.sess, self.generator, self.gen_data_loader)
+        #     end = time()
+        #     print('epoch:' + str(self.epoch) + '\t time:' + str(end - start))
+        #     self.add_epoch()
+        #     if epoch % 5 == 0:
+        #         #my_generate_samples(self.sess, self.generator, self.batch_size, self.generate_num, self.generator_file)
+        #         #get_real_test_file()
+        #         #self.evaluate()
+        #         saver.save(self.sess,'ckpt/lstm.ckpt',global_step=epoch)
+        saver=tf.train.Saver(max_to_keep=1)
+        model_file=tf.train.latest_checkpoint('ckpt/')
+        saver.restore(self.sess, model_file)
+        my_generate_samples(self.sess, self.generator, self.batch_size, self.sequence_length, self.generate_num, self.generator_file, g_x_file=self.g_x_file)
+        get_real_test_file()
 
 
 
